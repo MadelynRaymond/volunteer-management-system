6714856b45599fbd78291703c5f842b40333fd09
Madelyn Raymond <n01440101@unf.edu>
2022-07-26 00:29:40 -0400

    added simple volunteer crud

--- server/dist/index.js
+++ server/dist/index.js
@@ -4,12 +4,45 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
 const app = (0, express_1.default)();
+app.use(express_1.default.json());
 const port = 8080;
 app.get('/', (req, res) => {
-    res.send("Hello World.");
+    res.status(200).send({ message: 'hello, world' });
 });
 app.listen(port, () => {
     console.log(`Listening on ${port}`);
+});
+//Get all volunteers
+app.get('/Volunteers', async (req, res) => {
+    const volunteers = await prisma.user.findMany({
+        where: {
+            role: 'VOLUNTEER'
+        }
+    });
+    res.status(200).send(volunteers);
+});
+//Create volunteer
+app.post('/Volunteer', async (req, res) => {
+    const volunteer = await prisma.user.create({
+        data: {
+            username: req.body.username,
+            password: req.body.password,
+            role: 'VOLUNTEER'
+        }
+    });
+    res.status(201).send(volunteer);
+});
+//Delete volunteer
+app.delete('/Volunteer/:id', async (req, res) => {
+    const id = parseInt(req.params.id);
+    await prisma.user.delete({
+        where: {
+            id
+        }
+    });
+    res.sendStatus(203);
 });
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import { PrismaClient } from '@prisma/client';
+
+
+const prisma = new PrismaClient()
 
 const app = express()
+app.use(express.json())
 const port = 8080
 
 app.get('/', (req, res) => {
@@ -11,3 +16,41 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
     console.log(`Listening on ${port}`)
 } )
+
+//Get all volunteers
+app.get('/Volunteers', async (req, res) => {
+    
+    const volunteers = await prisma.user.findMany({
+        where: {
+            role: 'VOLUNTEER'
+        }
+    })
+
+    res.status(200).send(volunteers)
+
+})
+
+//Create volunteer
+app.post('/Volunteer', async (req, res) => {
+    const volunteer = await prisma.user.create({
+        data: {
+            username: req.body.username,
+            password: req.body.password,
+            role: 'VOLUNTEER'
+        }
+    })
+    res.status(201).send(volunteer)
+})
+
+
+//Delete volunteer
+app.delete('/Volunteer/:id', async (req, res) => {
+    const id = parseInt(req.params.id)
+    await prisma.user.delete({
+        where: {
+            id 
+        }
+    })
+    res.sendStatus(203)
+})
+
