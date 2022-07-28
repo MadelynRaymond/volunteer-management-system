25d6134f2aca4a99e955c7a187e753f0193a810a
mreid21 <n01437978@unf.edu>
2022-07-28 15:24:05 -0400

    added base table component

---
+++ app/src/components/BaseTable.tsx
@@ -0,0 +1,80 @@
+import React from 'react'
+import {
+    Table,
+    Thead,
+    Tbody,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+    Wrap,
+    Tag
+  } from '@chakra-ui/react'
+
+{/*TODO: Add Props to base table */}
+export default function BaseTable() {
+  return (
+    <TableContainer shadow={'md'} textAlign={'center'} borderRadius={'0.5rem'} p={2}>
+            <Table variant='simple' colorScheme='facebook'>
+              <TableCaption></TableCaption>
+              <Thead>
+                <Tr>
+                  <Th>Name</Th>
+                  <Th>Email</Th>
+                  <Th>Address</Th>
+                  <Th>Availability</Th>
+                  <Th>Approval Status</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+
+                <Tr>
+                  <Td>John Applebottom </Td>
+                  <Td>yaboijappleb218@gmail.com</Td>
+                  <Td>123 Sesame St. New York, NY 33523</Td>
+                  <Td>
+                    <Wrap>
+                      <Tag>11:00AM-2:30PM</Tag>
+                      <Tag>5:00PM-7:30PM</Tag>
+                    </Wrap>
+                  </Td>
+                  <Td>
+                    <Tag variant={'solid'} colorScheme={'green'}>Approved</Tag>
+                  </Td>
+                </Tr>
+
+                <Tr>
+                  <Td>Hozier Milton</Td>
+                  <Td>themilt99@outlook.com</Td>
+                  <Td>1250 Azure Ln. San Francisco, CA 90142</Td>
+                  <Td>
+                    <Wrap>
+                      <Tag>9:00AM-12:30PM</Tag>
+                      <Tag>2:00PM-2:30PM</Tag>
+                      <Tag>3:00PM-4:30PM</Tag>
+                    </Wrap>
+                  </Td>
+                  <Td>
+                    <Tag variant={'solid'} colorScheme={'yellow'}>Pending</Tag>
+                  </Td>
+                </Tr>
+
+                <Tr>
+                  <Td>Claire White</Td>
+                  <Td>cwhite@gmail.com</Td>
+                  <Td>740 Rock Ln. Jackson, MO 22142</Td>
+                  <Td>
+                    <Wrap>
+                      <Tag>3:00PM-8:30PM</Tag>
+                    </Wrap>
+                  </Td>
+                  <Td>
+                    <Tag variant={'solid'} colorScheme={'red'}>Denied</Tag>
+                  </Td>
+                </Tr>
+              </Tbody>
+            </Table>
+          </TableContainer>
+  )
+}
