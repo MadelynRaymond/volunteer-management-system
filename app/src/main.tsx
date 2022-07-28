4d5f4bf243f68b6cca20e2d10251c3fe99ff36a5
mreid21 <n01437978@unf.edu>
2022-07-28 15:44:24 -0400

    added prettier

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-import { ChakraProvider } from '@chakra-ui/react'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import { ChakraProvider } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ChakraProvider>
-    <App/>
+    <App />
   </ChakraProvider>
-)
+);
