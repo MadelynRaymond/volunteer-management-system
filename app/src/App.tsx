7c7ac68bd6612ffaab11a6660ed886268d20cb52
Madelyn <n01440101@unf.edu>
2022-07-27 23:17:59 -0400

    began adding volunteer form

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,4 +1,17 @@
 import React from 'react'
+import { Checkbox, CheckboxGroup } from '@chakra-ui/react'
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  Button
+} from '@chakra-ui/react'
+
 import {
     Box,
     Flex,
@@ -26,13 +39,71 @@ export default function App() {
                     <FormLabel>Last Name</FormLabel>
                     <Input type='text' />
                 </FormControl>
-
               </Flex>
+
               <FormControl>
                     <FormLabel>Email</FormLabel>
                     <Input type='email' />
               </FormControl>
+              <FormControl>
+                    <FormLabel>Address</FormLabel>
+                    <Input type='text' />
+                    
+              </FormControl>
             </Stack>
+            <Flex gap="3">
+              <FormControl>
+                <FormLabel>Approval Status</FormLabel>
+                <Select variant='filled' placeholder='Select Status'>
+                  <option value='option1'>Pending Approval</option>
+                  <option value='option2'>Approved</option>
+                  <option value='option3'>Disapproved</option>
+                </Select>
+              </FormControl>
+              <FormControl>
+                <FormLabel>Education Background</FormLabel>
+                <Select variant='filled' placeholder='Select Highest Education'>
+                  <option value=''>Less Than Highschool Diploma</option>
+                  <option value=''>Highschool Diploma</option>
+                  <option value=''>Some College</option>
+                  <option value=''>...</option>
+                </Select>
+              </FormControl>
+            </Flex>
+            <FormControl>
+            <FormLabel>Test </FormLabel>
+            <Flex gap="3">
+              <Menu closeOnSelect={false}>
+                <MenuButton w="295px" as={Button}>
+                  Preferred Centers
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>
+                      <Checkbox colorScheme='purple'>NE FL Environmental Outlook Center</Checkbox>
+                  </MenuItem>
+                  <MenuItem>
+                      <Checkbox colorScheme='purple'>Jacksonville Women's Center</Checkbox>
+                  </MenuItem>
+                  <MenuItem>
+                      <Checkbox colorScheme='purple'>Duval Humane Society</Checkbox>
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+              <Menu closeOnSelect={false}>
+                <MenuButton w="295px" as={Button}>
+                  Current Licenses
+                </MenuButton>
+                <MenuList>
+                  <MenuItem>
+                      <Checkbox colorScheme='purple'>FL Drivers License</Checkbox>
+                  </MenuItem>
+                  <MenuItem>
+                      <Checkbox></Checkbox>
+                  </MenuItem>
+                </MenuList>
+              </Menu>
+            </Flex>
+            </FormControl>
         </Box>
     </Flex>
   )
