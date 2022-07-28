1e5465bcd9b1ff6dc381d9a0d574c5172ba02c3e
mreid21 <n01437978@unf.edu>
2022-07-28 19:05:43 -0400

    added multiselect

---
+++ app/src/components/MultiSelect.tsx
@@ -0,0 +1,84 @@
+import {
+  Box,
+  Input,
+  Stack,
+  Tag,
+  TagLabel,
+  TagRightIcon,
+  Wrap,
+} from "@chakra-ui/react";
+import { CloseIcon } from "@chakra-ui/icons";
+import React from "react";
+
+const TagWithIcon: React.FC<{
+  name: string;
+  handleClick: (option: string) => void;
+}> = (props) => {
+  return (
+    <Tag onClick={() => props.handleClick(props.name)} size={"md"}>
+      <TagLabel>{props.name}</TagLabel>
+      <TagRightIcon boxSize={"0.5em"} as={CloseIcon} />
+    </Tag>
+  );
+};
+
+const MultiSelectOption: React.FC<{
+  option: string;
+  handleSelect: (option: string) => void;
+}> = ({ option, handleSelect }) => {
+  return (
+    <Box
+      pt={1}
+      pb={1}
+      onMouseDown={() => handleSelect(option)}
+      _hover={{ backgroundColor: "gray.100" }}
+    >
+      <Box pl={3}>{option}</Box>
+    </Box>
+  );
+};
+
+export default function MultiSelect({ options }: { options: string[] }) {
+
+  const [show, setShow] = React.useState(false);
+  const [selected, setSelected] = React.useState<string[]>([]);
+  const [filteredOptions, setFilteredOptions] = React.useState(options);
+
+
+  const handleClick = (clickedTag: string) => {
+    setSelected(selected.filter((s) => s !== clickedTag));
+    setFilteredOptions([...filteredOptions, clickedTag]);
+  };
+
+  const handleSelect = (selectedOption: string) => {
+    setSelected([...selected, selectedOption]);
+    setFilteredOptions(filteredOptions.filter((s) => s !== selectedOption));
+  };
+
+  return (
+    <Stack mt={'0.5rem'} spacing="0.5rem">
+      <Box>
+        <Wrap>
+          {selected.map((s) => (
+            <TagWithIcon handleClick={handleClick} name={s} />
+          ))}
+        </Wrap>
+      </Box>
+      <Input
+        onBlur={() => setShow(false)}
+        onFocus={() => setShow(true)}
+        placeholder="choose centers"
+        type="text"
+      />
+      <Box
+        display={show && filteredOptions.length > 0 ? "block" : "none"}
+        pb={1}
+        shadow={"md"}
+      >
+        {filteredOptions.map((option) => (
+          <MultiSelectOption handleSelect={handleSelect} option={option} />
+        ))}
+      </Box>
+    </Stack>
+  );
+}
