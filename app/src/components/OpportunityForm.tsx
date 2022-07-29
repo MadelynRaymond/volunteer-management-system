7ce01a6d51f646e3abf575836d6baadff738e178
mreid21 <n01437978@unf.edu>
2022-07-29 19:38:09 -0400

    added form validation to create opportunity

--- app/src/components/OpportunityForm.tsx
+++ app/src/components/OpportunityForm.tsx
@@ -1,12 +1,16 @@
 import { CheckIcon, CloseIcon } from '@chakra-ui/icons'
-import { Box, Button, Flex, FormLabel, Input, Select, Stack, Textarea } from '@chakra-ui/react'
+import { Box, Button, Flex, FormControl, FormLabel, Input, Select, Stack, Textarea } from '@chakra-ui/react'
 import axios from 'axios'
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useNavigate } from 'react-router-dom'
+import { interests } from '../assets/interests'
 import MultiSelect from './MultiSelect'
 
 export default function OpportunityForm() {
 
+  const navigate = useNavigate()
+
+  const [submitted, setSubmitted] = React.useState(false)
   const [centers, setCenters] = React.useState<Array<{id: number, name: string}>>()
   const [name, setName] = React.useState('')
   const [address, setAddress] = React.useState('')
@@ -38,13 +42,34 @@ export default function OpportunityForm() {
     const {data} = await axios.post('http://localhost:8080/Opportunities', opportunity)
   }
 
+  const submit = () => {
+    setSubmitted(true)
+
+    const timeout = setTimeout(() => {
+      setSubmitted(false)
+      clearInterval(timeout)
+    }, 1000 * 5)
+
+    const hasErrors = [name, center, address, desc, startTime, endTime].some(field => field === '')
+
+    if(!hasErrors){
+      postOpportunity()
+      navigate('/')
+    }
+
+  }
+
+  const hasError = (input: string) => submitted && input === ''
+
   return (
     <Flex w="100vw" mt="2rem" justifyContent="center">
       <Box w="700px">
         <Stack spacing='1.25rem'>
 
-            <FormLabel>Opportunity Name:</FormLabel>
-            <Input onChange={(e) => setName(e.target.value)} type="text"/>
+            <FormControl isInvalid={hasError(name)} isRequired>
+              <FormLabel>Opportunity Name:</FormLabel>
+              <Input onChange={(e) => setName(e.target.value)} type="text"/>
+            </FormControl>
 
             <Flex gap='5' alignItems='end'>
                   <Box w='100%'>
@@ -52,58 +77,53 @@ export default function OpportunityForm() {
                         <MultiSelect
                         onSelect={(e) => setTags(e)}
                         placeholder="Select Skills/Interests"
-                        options={[
-                            {id: 1, value: "Animal Care"},
-                            {id: 2, value: "Event Planning"},
-                            {id: 3, value: "Technology"},
-                            {id: 4, value: "Working with Children"},
-                            {id: 5, value: "Sports"},
-                            {id: 6, value: "Healthcare"},
-                            {id: 7, value: "Community Development"},
-                            {id: 8, value: "Education"},
-                            {id: 9, value: "Baking"},
-                            {id: 10, value: "Environmental Issues"},
-                            {id: 11, value: "Women's Empowerment"},
-                            {id: 12, value: "Politics"},
-                            {id: 13, value: "Diversity & Equity"},
-                            {id: 14, value: "Agriculture"},
-                            {id: 15, value: "Wildlife Protection"}
-                        ]}
+                        options={interests}
                     />
                  </Box>
                   <Box w='100%'>
-                    <FormLabel>Associated Center</FormLabel>
-                    <Stack spacing={3}>
-                        <Select onChange={(e) => setCenter(e.target.value)} variant='filled' placeholder='Select Center' >
-                            {centers && centers.map((center) => <option value={center.id} key={center.id}>{center.name}</option>)}
-                        </Select>
-                    </Stack>
+                    <FormControl isInvalid={hasError(center)} isRequired>
+                      <FormLabel>Associated Center</FormLabel>
+                      <Stack spacing={3}>
+                          <Select onChange={(e) => setCenter(e.target.value)} variant='filled' placeholder='Select Center' >
+                              {centers && centers.map((center) => <option value={center.id} key={center.id}>{center.name}</option>)}
+                          </Select>
+                      </Stack>
+                    </FormControl>
                   </Box>
                 </Flex>
                 <Flex gap='5' alignItems='center'>
                   <Box w='100%'>
-                    <FormLabel>Start Time</FormLabel>
-                    <Input onChange={(e) => setStartTime(e.target.value)} type="text" placeholder="8AM"/>
+                    <FormControl isInvalid={hasError(startTime)} isRequired>
+                      <FormLabel>Start Time</FormLabel>
+                      <Input onChange={(e) => setStartTime(e.target.value)} type="text" placeholder="8AM"/>
+                    </FormControl>
                  </Box>
                   <Box w='100%'>
-                    <FormLabel>End Time</FormLabel>
-                    <Input onChange={(e) => setEndTime(e.target.value)} type="text" placeholder="12PM"/>
+                    <FormControl isInvalid={hasError(endTime)} isRequired>
+                      <FormLabel>End Time</FormLabel>
+                      <Input onChange={(e) => setEndTime(e.target.value)} type="text" placeholder="12PM"/>
+                    </FormControl>
                   </Box>
                 </Flex>
 
-            <FormLabel>Location Address:</FormLabel>
-            <Input onChange={(e) => setAddress(e.target.value)} type="text" placeholder="117 W. Duval St., Suite 210, Jacksonville, FL 32202"/>
-            <FormLabel>Description:</FormLabel>
-            <Textarea onChange={(e) => setDesc(e.target.value)} placeholder='Please give a brief description of the opportunity for prospective volunteers.' />
+            <FormControl isInvalid={hasError(address)} isRequired>
+              <FormLabel>Location Address:</FormLabel>
+              <Input onChange={(e) => setAddress(e.target.value)} type="text" placeholder="117 W. Duval St., Suite 210, Jacksonville, FL 32202"/>
+            </FormControl>
+
+            <FormControl isInvalid={hasError(desc)} isRequired>
+              <FormLabel>Description:</FormLabel>
+              <Textarea onChange={(e) => setDesc(e.target.value)} placeholder='Please give a brief description of the opportunity for prospective volunteers.' />
+            </FormControl>
           
             <Flex gap='5' justifyContent='center'>
                 <NavLink to="/"><Button leftIcon={<CloseIcon />} colorScheme='purple' variant='solid'>
                 Cancel
                 </Button></NavLink>
                 
-                <NavLink to="/"><Button onClick={postOpportunity} colorScheme='pink' leftIcon={<CheckIcon />}  variant='solid'>
+             <Button onClick={submit} colorScheme='pink' leftIcon={<CheckIcon />}  variant='solid'>
                 Submit
-                </Button></NavLink>
+              </Button>
             </Flex>
         </Stack>
       </Box>
