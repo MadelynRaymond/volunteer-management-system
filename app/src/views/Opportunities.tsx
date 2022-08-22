18cb4f092c01b62fa52087b007d0e4853a8468fd
Madelyn Raymond <n01440101@unf.edu>
2022-08-22 19:24:31 -0400

    changed icon on opportunity table

--- app/src/views/Opportunities.tsx
+++ app/src/views/Opportunities.tsx
@@ -1,4 +1,4 @@
-import { AddIcon, ArrowBackIcon } from '@chakra-ui/icons'
+import { AddIcon, ArrowBackIcon, CloseIcon } from '@chakra-ui/icons'
 import { Box, Text, Button, Center, Flex, Stack } from '@chakra-ui/react'
 import React from 'react'
 import { NavLink } from 'react-router-dom'
@@ -14,7 +14,7 @@ export default function Opportunities() {
               <Flex justifyContent={'space-between'} gap={'1rem'}>
                 <Text fontSize={'3xl'}>Opportunities</Text>
                 <Flex gap={'1rem'}>
-                  <NavLink to='/'><Button leftIcon={<ArrowBackIcon />} colorScheme={'red'}>Back</Button></NavLink>
+                  <NavLink to='/'><Button leftIcon={<CloseIcon />} colorScheme={'red'}>Back</Button></NavLink>
                   <NavLink to='/CreateOpportunity'><Button w={'170px'} rightIcon={<AddIcon />}  colorScheme={'purple'}>Add Opportunity</Button></NavLink>
                 </Flex>
             </Flex>
