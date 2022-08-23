fc22f3c94f446773a36f29bc8c44825a1005ea64
Matt <n01437978@unf.edu>
2022-08-23 17:40:12 -0400

    partial login

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,49 @@
 import React from "react";
-import {
-  Box,
-  Checkbox,
-  Flex,
-  FormControl,
-  FormLabel,
-  Input,
-  Stack,
-} from "@chakra-ui/react";
+import "./index.css";
+import { BrowserRouter, Route, Routes, useNavigate } from "react-router-dom";
+import PersonalInfoForm from "./components/PersonalInfoForm";
+import VolunteerInfoForm from "./components/VolunteerInfoForm";
+import EmergencyContactForm from "./components/EmergencyContactForm";
+import NewVolunteer from "./views/NewVolunteer";
+import Volunteers from "./views/Volunteers";
+import EditVolunteer from "./views/EditVolunteer";
+import Opportunities from "./views/Opportunities";
+import Login from "./views/Login";
+import VolunteerMatches from "./views/VolunteerMatches";
+import OpportunityMatches from "./views/OpportunityMatches";
+import AdminPage from "./components/AdminPage";
+import OpportunityForm from "./components/OpportunityForm";
+import { Box, Button, Flex } from "@chakra-ui/react";
+import Nav from "./components/Nav";
 
-import MultiSelect from "./components/MultiSelect";
-import FormPage from "./views/FormPage";
 
 export default function App() {
-  return <div></div>;
+  
+
+  return (
+    <>
+      <BrowserRouter>
+        <Nav></Nav>
+        <Routes>
+          <Route path="/" element={<AdminPage />} />
+          <Route path="/Login" element={<Login />} />
+          <Route path="/Opportunities" element={<Opportunities />} />
+          <Route path="/Opportunities/VolunteerMatches/:id" element={<VolunteerMatches />} />
+          <Route path="/Volunteers" element={<Volunteers />} />
+          <Route path="/Volunteers/OpportunityMatches/:id" element={<OpportunityMatches />} />
+          <Route path="/EditVolunteer/:id" element={<EditVolunteer/>}>
+            <Route path="1" element={<PersonalInfoForm />} />
+            <Route path="2" element={<VolunteerInfoForm />} />
+            <Route path="3" element={<EmergencyContactForm />} />
+          </Route>
+          <Route path="/CreateOpportunity" element={<OpportunityForm />} />
+          <Route path="/CreateVolunteer" element={<NewVolunteer />}>
+            <Route path="1" element={<PersonalInfoForm />} />
+            <Route path="2" element={<VolunteerInfoForm />} />
+            <Route path="3" element={<EmergencyContactForm />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
 }
--- app/src/main.tsx
+++ app/src/main.tsx
@@ -3,45 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import PersonalInfoForm from "./components/PersonalInfoForm";
-import VolunteerInfoForm from "./components/VolunteerInfoForm";
-import EmergencyContactForm from "./components/EmergencyContactForm";
-import NewVolunteer from "./views/NewVolunteer";
 import StoreProvider from "./context/store";
-import Volunteers from "./views/Volunteers";
-import EditVolunteer from "./views/EditVolunteer";
-import Opportunities from "./views/Opportunities";
-import Login from "./views/Login";
-import VolunteerMatches from "./views/VolunteerMatches";
-import OpportunityMatches from "./views/OpportunityMatches";
-import OpportunityForm from "./Components/OpportunityForm";
-import AdminPage from "./Components/AdminPage";
+
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <ChakraProvider>
     <StoreProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<AdminPage />} />
-          <Route path="/Login" element={<Login />} />
-          <Route path="/Opportunities" element={<Opportunities />} />
-          <Route path="/Opportunities/VolunteerMatches/:id" element={<VolunteerMatches />} />
-          <Route path="/Volunteers" element={<Volunteers />} />
-          <Route path="/Volunteers/OpportunityMatches/:id" element={<OpportunityMatches />} />
-          <Route path="/EditVolunteer/:id" element={<EditVolunteer/>}>
-            <Route path="1" element={<PersonalInfoForm />} />
-            <Route path="2" element={<VolunteerInfoForm />} />
-            <Route path="3" element={<EmergencyContactForm />} />
-          </Route>
-          <Route path="/CreateOpportunity" element={<OpportunityForm />} />
-          <Route path="/CreateVolunteer" element={<NewVolunteer />}>
-            <Route path="1" element={<PersonalInfoForm />} />
-            <Route path="2" element={<VolunteerInfoForm />} />
-            <Route path="3" element={<EmergencyContactForm />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
+      <App/>
     </StoreProvider>
   </ChakraProvider>
 );
